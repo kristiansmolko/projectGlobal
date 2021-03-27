76a93d81a97521f9906bb5d8f78a39a90001d0eb
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-27 10:39:06 +0100

    Added getConnection method to connect to database

--- src/main/java/global/database/Database.java
+++ src/main/java/global/database/Database.java
@@ -1,4 +1,29 @@
 package global.database;
 
+import global.log.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Database {
+    Log log = new Log();
+
+    public Connection getConnection() throws IOException, SQLException {
+        Properties prop = new Properties();
+        InputStream loader = getClass().getClassLoader().getResourceAsStream("data.properties");
+        prop.load(loader);
+        String url = prop.getProperty("url");
+        String user = prop.getProperty("user");
+        String pass = prop.getProperty("password");
+        Connection connection = DriverManager.getConnection(url, user, pass);
+        if (connection != null){
+            log.ok("Connection established");
+            return connection;
+        }
+        return null;
+    }
 }
