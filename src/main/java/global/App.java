9a20e8b2e22ef1c6c1f9bc637d8a3188137f9de4
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-27 11:01:08 +0100

    Modified app class to start server

--- src/main/java/global/App.java
+++ src/main/java/global/App.java
@@ -1,4 +1,13 @@
 package global;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "global.controller")
 public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
